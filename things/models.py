b5415aeb0df8e9b6999aaf440f28712b7f11123f
Darren Sandhu <darrensandhu@Darrens-MBP.localdomain>
2022-10-31 19:51:10 +0000

    Refine thing model with restrictions.

--- things/models.py
+++ things/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Thing(AbstractUser):
-    name = models.CharField()
-    descirption = models.CharField()
-    quantity = models.IntegerField()
+# Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length = 30, unique = True, blank = False)
+    description = models.CharField(max_length = 120, blank = True)
+    quantity = models.IntegerField(
+        validators = [
+            MinValueValidator(0), 
+            MaxValueValidator(100)
+        ] 
+    )
